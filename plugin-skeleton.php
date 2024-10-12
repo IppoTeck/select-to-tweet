f0fd752bd1fbcca11801cfbea0fd805cc81822d3
pratik-londhe4 <pratik.londhe@rtcamp.com>
2024-10-12 10:06:09 +0530

    Create class for the plugin

--- plugin-skeleton.php
+++ plugin-skeleton.php
@@ -1,10 +1,33 @@
 <?php
 /**
- * Plugin Name: Plugin Skeleton
- * Plugin URI: hhttps://github.com/IppoTeck/plugin-skeleton
- * Description: A skeleton plugin for WordPress development.
+ * Plugin Name: Select to tweet
+ * Plugin URI: hhttps://github.com/IppoTeck/select-to-tweet
+ * Description: Select to tweet is a simple plugin that allows visitors to select text on your website and tweet it.
  * Version: 1.0.0
  * Author: IppoTeck
  * Author URI: ippoteck.in/
  * License: GPL2
  */
+
+ class SelectToTweet {
+	private $plugin_name;
+	private $version;
+
+	public function __construct() {
+		$this->plugin_name = 'select-to-tweet';
+		$this->version = '1.0.0';
+		$this->define_hooks();
+	}
+
+	private function define_hooks() {
+		add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
+	}
+
+	public function enqueue_scripts() {
+		wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'JS/main.js', array('jquery'), $this->version, true);
+	}
+
+}
+
+
+$select_to_tweet = new SelectToTweet();
